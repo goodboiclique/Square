68d3f3df70e58a160aca98a46665e63f8f830bc0
Artem <sploditelartem@gmail.com>
2022-05-12 11:43:36 +0300

    Square site v1.0

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+   $('.icon-menu').click(function(event){
+      $('.icon-menu, .menu__body').toggleClass('active')
+      $('body').toggleClass('lock')
+      $('.slider').slick({
+         arrows:false,
+         dots:true,
+         adaptiveHeight: true
+      });
+   });
+});
+function ibg(){
+   $.each($('.ibg'), function(index, val) {
+   if($(this).find('img').length>0){
+   $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+   }
+   });
+   }
+   ibg();
+   $(document).ready(function(){
+      $('.slider').slick({
+         arrows:false,
+         dots:true,
+         adaptiveHeight: true,
+      });
+   });
